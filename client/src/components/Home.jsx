0eb2f7895c8a4d8987db69a23500e9f3547b3460
Milecia McG <flippedcoding@gmail.com>
2019-11-12 19:50:31 -0600

    initial dashboard layout

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+
+import '../css/Home.css';
 
 class Home extends Component {
-
 
     render() {
         return (
             <div className="container">
-                home works
+                <div id="month-to-date-total">
+                    <p>Spent this month</p>
+                    <p>$123.45</p>
+                </div>
+                <div id="month-to-date-graph">
+                    graph
+                </div>
+                <footer className="flex-2">
+                    <div className="add-item center-content">
+                        <p>Add Item</p>
+                        <p>
+                            <FontAwesomeIcon icon={faPlusSquare}
+                                id="add-item-icon" />
+                        </p>
+                    </div>
+                    <div className="graph-stats center-content">
+                        <p>See Other Stats</p>
+                        <div className="flex-2">
+                            <p>By Type</p>
+                            <p>By Week</p>
+                        </div>
+                    </div>
+                </footer>
             </div>
         );
     }
