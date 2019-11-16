f5312756e44be4719eb177b5ef285fc00b3939e7
flippedcoder <flippedcoding@gmail.com>
2019-11-16 13:00:04 -0600

    added graph to home screen; made initial states for future component props

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { VictoryPie } from 'victory';
 
 import '../css/Home.css';
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showItemModal: false,
+            statsGraphType: 'week'
+        };
+        this.showAddItemModal = this.showAddItemModal.bind(this);
+        this.showStatsByType = this.showStatsByType.bind(this);
+        this.showStatsByWeek = this.showStatsByWeek.bind(this);
+    }
+
+    showAddItemModal() {
+        let showItemModal = this.state.showItemModal;
+        this.setState({
+            showItemModal: !showItemModal
+        });
+    }
+
+    showStatsByType() {
+        this.setState({
+            statsGraphType: 'type'
+        });
+    }
+
+    showStatsByWeek() {
+        this.setState({
+            statsGraphType: 'week'
+        });
+    }
 
     render() {
         return (
@@ -14,21 +44,34 @@ class Home extends Component {
                     <p>$123.45</p>
                 </div>
                 <div id="month-to-date-graph">
-                    graph
+                    <div className="graph-container">
+                        <VictoryPie
+                            colorScale={["tomato", "gold", "navy"]}
+                            cornerRadius={7}
+                            // TODO: get data array from App.jsx
+                            // data={this.props.data}
+                            data={[
+                                { x: "Food", y: 27 },
+                                { x: "Bills", y: 42 },
+                                { x: "Stuff", y: 31 }
+                            ]}
+                            height={300}
+                            innerRadius={50}
+                        />
+                    </div>
                 </div>
                 <footer className="flex-2">
                     <div className="add-item center-content">
                         <p>Add Item</p>
-                        <p>
-                            <FontAwesomeIcon icon={faPlusSquare}
-                                id="add-item-icon" />
+                        <p id="add-item-icon" onClick={this.showAddItemModal}>
+                            <FontAwesomeIcon icon={faPlusSquare} />
                         </p>
                     </div>
                     <div className="graph-stats center-content">
                         <p>See Other Stats</p>
                         <div className="flex-2">
-                            <p>By Type</p>
-                            <p>By Week</p>
+                            <p onClick={this.showStatsByType}>By Type</p>
+                            <p onClick={this.showStatsByWeek}>By Week</p>
                         </div>
                     </div>
                 </footer>
